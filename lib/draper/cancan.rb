b0053b89a323def46db35510a137fe777d2257f7
Robert Mathews <rob@justsoftwareconsulting.com>
2015-11-05 17:40:30 -0500

    @wip cleanup for initial release.

--- lib/draper/cancan.rb
+++ lib/draper/cancan.rb
@@ -1,11 +1,9 @@
 require "draper/cancan/version"
 
-puts "Draper::CanCan being loaded!"
 module Draper
   module CanCan
     # actually don't need any code in my gem's namespace .. 
     def can?(action, subject, *extra_args)
-      puts "Draper::CanCan#can? was called!"
       if subject.is_a?(Draper::Decorator)
         super(action,subject.model,*extra_args)
       else
@@ -14,7 +12,6 @@ module Draper
     end
 
     def cannot?(*args)
-      puts "Draper::CanCan#cannot?() was called"
       !can?(*args)
     end
   end
